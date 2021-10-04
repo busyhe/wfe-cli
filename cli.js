9a6dc57d4ca8b8226332a027e94a4b55fc23ec3a
busyhe <525118368@qq.com>
2021-10-04 14:14:10 +0800

    feat: add templates list

--- cli.js
+++ cli.js
@@ -11,20 +11,32 @@ const ini = require('ini');
 const chalk = require('chalk');
 const {Command} = require('commander');
 const program = new Command();
+const package = require('./package.json')
 const {isWindows} = require('./utils');
-const logger = require('./utils/log');
+const logger = require('./lib/logger');
 const LOCAL_REPOS = require('./repos.json');
+const request = require('request');
+const dayjs = require('dayjs');
+const relativeTime = require('dayjs/plugin/relativeTime');
+dayjs.extend(relativeTime);
 
 const HOME_PATH = process.env[isWindows() ? 'USERPROFILE' : 'HOME'];
 const WFE_CLI_REPOS_RC = path.join(HOME_PATH, '.wfe_cli_repos_rc');
+const DEFAULT_REPO = 'wfe';
 
-program.version(require('./package').version);
+program.version(package.version);
 
 program.usage('<command> [options]');
 
-program.command('init', 'generate a new project from a template');
+program
+    .description('generate a new project from a template')
+    .command('init')
+    .action(initProject)
 
-program.command('list', 'list available official templates');
+program
+    .description('list available official templates')
+    .command('list')
+    .action(showTemplates);
 
 const reposProgram = program.command('repos');
 
@@ -50,22 +62,62 @@ process.on('exit', () => {
     console.log();
 });
 
+function initProject() {
+}
+
+/**
+ * 显示模板列表
+ */
+function showTemplates() {
+    const currentRepo = getCurrentRepo();
+    request({
+        url: currentRepo.repos,
+        headers: {
+            'User-Agent': package.name
+        }
+    }, (err, res, body) => {
+        if (err) logger.fatal(err);
+        const requestBody = JSON.parse(body);
+        if (Array.isArray(requestBody)) {
+            console.log();
+            console.log('  Available templates:');
+            console.log();
+            requestBody.forEach(repo => {
+                console.log(
+                    '  ' + chalk.yellow('★') +
+                    '  ' + chalk.blue(repo.name) +
+                    ' - ' + repo.description +
+                    '  ' + chalk.gray(dayjs(repo.updated_at || repo.last_activity_at).fromNow()));
+            });
+            console.log();
+            res.headers['x-ratelimit-remaining'] && console.log(chalk.gray(`  Ratelimit-Remaining: ${res.headers['x-ratelimit-remaining']}`));
+        } else {
+            // 速率限制
+            if (requestBody.message.indexOf('API rate limit exceeded') >= 0) {
+                console.log(chalk.yellow(`ratelimit-reset: ${dayjs.unix(res.headers['x-ratelimit-reset']).format('YYYY-MM-DD HH:mm:ss')}`));
+                console.log();
+            }
+            console.log(chalk.red(requestBody.message));
+        }
+    });
+}
+
 /**
  * 显示repos列表
  */
 function showRepos() {
     const infos = [];
     const allRepos = getRepos();
-    const currentRepo = getCurrentRepo();
+    const {current} = getCustomRepos();
     const keys = Object.keys(allRepos);
     const len = Math.max(...keys.map(key => key.length)) + 1;
     const templateLen = Math.max(...Object.values(allRepos).map(i => i.templates.length));
     console.log();
-    console.log('  Available templates:');
+    console.log('  Available Repos:');
     console.log();
     Object.keys(allRepos).forEach(function (key) {
         const item = allRepos[key];
-        const prefix = currentRepo === key ? '* ' : '  ';
+        const prefix = current === key ? '* ' : '  ';
         infos.push('  ' + chalk.yellow(prefix) + ' ' + chalk.blue(key) + line(key, len) + item.templates + line(item.templates, templateLen) + item.repos);
     });
     infos.push('');
@@ -83,13 +135,14 @@ function useRepo(repoName) {
     if (!allRepos[repoName]) {
         console.log('');
         logger.error('Not find repo ' + repoName);
-        return
+        return;
     }
     let customRepos = getCustomRepos();
     customRepos = Object.assign({}, customRepos, {
         current: repoName
-    })
-    setCustomRepos(customRepos)
+    });
+    setCustomRepos(customRepos);
+    logger.log(repoName + ' : ' + allRepos[repoName].repos)
 }
 
 /**
@@ -103,15 +156,14 @@ function addRepo(repoName, repo, template = repoName) {
     if (allRepos[repoName]) {
         console.log('');
         logger.error(`repo ${repoName} is existed`);
-        return
+        return;
     }
     const customRepos = getCustomRepos();
-    console.log(customRepos);
     customRepos.repos[repoName] = {
         templates: template,
         repos: repo
-    }
-    setCustomRepos(customRepos)
+    };
+    setCustomRepos(customRepos);
 }
 
 /**
@@ -123,13 +175,15 @@ function getRepos() {
     return Object.assign({}, LOCAL_REPOS, customRepos);
 }
 
-/**
- * 获取当前选择的repo
- * @returns {string}
- */
 function getCurrentRepo() {
-    const {current} = getCustomRepos();
-    return current;
+    const {current, repos = {}} = getCustomRepos();
+    const allRepos = getRepos();
+    if (allRepos[current]) return allRepos[current];
+    setCustomRepos({
+        current: DEFAULT_REPO,
+        repos
+    });
+    return LOCAL_REPOS[DEFAULT_REPO];
 }
 
 function line(str, len) {
@@ -142,10 +196,12 @@ function line(str, len) {
  * @returns {*|{current: string, repos: {}}}
  */
 function getCustomRepos() {
-    return fs.existsSync(WFE_CLI_REPOS_RC) ? ini.parse(fs.readFileSync(WFE_CLI_REPOS_RC, 'utf-8')) : {
-        current: 'wfe',
+    const customRepos = fs.existsSync(WFE_CLI_REPOS_RC) ? ini.parse(fs.readFileSync(WFE_CLI_REPOS_RC, 'utf-8')) : {
+        current: DEFAULT_REPO,
         repos: {}
     };
+    if (!customRepos.repos) customRepos.repos = {};
+    return customRepos;
 }
 
 /**
@@ -153,6 +209,5 @@ function getCustomRepos() {
  * @param repo
  */
 function setCustomRepos(repos) {
-    console.log('setCustomRepos', repos);
     fs.writeFileSync(WFE_CLI_REPOS_RC, ini.stringify(repos));
 }
